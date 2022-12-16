e0d5bf436c56555566f259bfde63ed82c37ea585
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2022-12-16 22:01:33 +0530

    FIX: add redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
+import Login from "./components/Login/Login"
 import { useSelector } from 'react-redux';
 import {selectUser} from "./features/userSlice"
 
@@ -12,12 +13,13 @@ function App() {
     <div className="app">
       <Header />
 
-      {!user ? <Login />:(
-        <div className="app__body">
-          <Sidebar />
-          <Feed />
-        </div>
-      )}
+      {!user ? (<Login />) : (
+          <div className='app__body'>
+            <Sidebar />
+            <Feed />
+          </div>
+        )
+      }
     </div>
   );
 }

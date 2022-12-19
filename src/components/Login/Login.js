cacbb37d005a8e6ba42d6403c310dc8bc8b1c19c
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2022-12-19 23:39:16 +0530

    add register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
+import { auth } from '../firebase';
+import { useDispatch } from 'react-redux';
+import {login} from '../../features/userSlice';
+import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [pic, setPic] = useState("");
+  const dispatch = useDispatch();
+
+
+  const loginToApp = (e) =>{
+    e.preventDefault();
+
+  };
+
+  const register = async() =>{
+    if(!name){
+      return alert("Please enter full name");
+    }
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then((userAuth)=>{
+      updateProfile(userAuth.user,{
+        displayName: name, 
+        photoUrl: pic
+      })
+      .then(()=>{
+        dispatch(login({
+          email: userAuth.user.email,
+          uid: userAuth.user.uid,
+          displayName: name,
+          photoUrl: pic,
+        }))
+      })
+    })
+    .catch(error=> alert(error));
+  };
+
   return (
     <div className='login'>
-        <h1>Log in</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png" alt="" />
+        <form>
+        <input type="text" name="" id="" placeholder='Full Name' value={name} onChange={(e)=>setName(e.target.value)} />
+        <input type="text" name="" id="" placeholder='Profile pic URL' value={pic} onChange={(e)=>setPic(e.target.value)}/>
+        <input type="email" name="" id="" placeholder='Email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+        <input type="password" name="" id="" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        <button type="submit" onClick={loginToApp}>Sign In</button>
+        </form>
+        <p>Not a member?
+        <span className='login__register' onClick={register}>Register Now</span>
+        </p>
     </div>
   )
 }
